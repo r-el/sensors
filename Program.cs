5b740e69b5dc40b6a41a417f2c01327498773b95
Ariel <arieltanami122@gmail.com>
2025-06-15 12:42:18 +0300

    Add Sensor class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using sensors.Entities;
+
+Sensor[] sensors = [new("Temperature-Sensor-01", "Temperature"), new("Motion-Sensor-02", "Motion")];
+
+Console.WriteLine("Initial status:");
+for (int i = 0; i < sensors.Length; i++)
+    Console.WriteLine($"Sensor #{i + 1}: {sensors[i]}");
+
+Console.WriteLine("\n--- Activating sensors ---");
+sensors[0].Activate();
+sensors[1].Activate();
+
+Console.WriteLine("\nStatus after activation:");
+for (int i = 0; i < sensors.Length; i++)
+    Console.WriteLine($"Sensor #{i + 1}: {sensors[i]}");
