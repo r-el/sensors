af8456ed56207de87c352390a8e3271120855b02
Ariel <arieltanami122@gmail.com>
2025-06-17 22:19:04 +0300

    Clean up Sensor.Activate() method - remove individual logging
    - Remove console output from Sensor.Activate() - logging now handled at higher level
    - Simplify method to single line for better readability
    - Activation feedback is now managed by BaseAgent.ActivateInactiveSensors()

--- Entities/Sensor.cs
+++ Entities/Sensor.cs
@@ -5,16 +5,11 @@ namespace sensors.Entities
 {
     public class Sensor(SensorType type) : BaseSensor(type)
     {
-        public override void Activate()
-        {
-            IsActive = true;
-            Console.WriteLine($"Activating sensor: {Type}");
-        }
+        public override void Activate() { IsActive = true; }
 
         public override bool Equals(object? obj)
             => obj is Sensor other && Type.Equals(other.Type);
 
-        public override int GetHashCode()
-            => Type.GetHashCode();
+        public override int GetHashCode() => Type.GetHashCode();
     }
 }
